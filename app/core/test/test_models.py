2d7a9dd97ba0833386bdad551f2f47052b80cf58
Charles Wood <charlesctwood@gmail.com>
2019-12-20 21:36:58 -0500

    Added list tags endpoint

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -1,11 +1,18 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='test@example.com', password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTest(TestCase):
 
     def test_create_user_with_email_successful(self):
-        '''test creating a user with an email is successful'''
+        """test creating a user with an email is successful"""
         email = "test@testemail.com"
         password = "Testpassword123"
         user = get_user_model().objects.create_user(
@@ -35,3 +42,12 @@ class ModelTest(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
