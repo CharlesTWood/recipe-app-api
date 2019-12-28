dda5a50a1830aa6e28de46a818c56ab997425cd3
Charles Wood <charlesctwood@gmail.com>
2019-12-28 17:45:06 -0500

    Added ingredients api

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -51,3 +51,13 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Saffron'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
